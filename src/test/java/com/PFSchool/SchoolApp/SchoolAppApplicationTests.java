8f9aea3de398b6f5d2d1430baf827515d6fcfbb3
Zayad <zayadk10@gmail.com>
2021-07-26 15:03:28 +0100

    Initialized spring project to start.

---
+++ src/test/java/com/PFSchool/SchoolApp/SchoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PFSchool.SchoolApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
