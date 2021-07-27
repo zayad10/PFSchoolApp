460c2b2d9e94dbd948f9c2d21b691924e29c6d03
Zayad <zayadk10@gmail.com>
2021-07-28 00:45:36 +0100

    Added methods for finding staff members by their department and their role.

--- src/main/java/com/PFSchool/SchoolApp/Staff/StaffRepository.java
+++ src/main/java/com/PFSchool/SchoolApp/Staff/StaffRepository.java
@@ -1,9 +1,15 @@
 package com.PFSchool.SchoolApp.Staff;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StaffRepository extends JpaRepository<Staff, Integer> {
+	
+	public List<Staff> findByDepartment(Department dept);
+	
+	public List<Staff> findByRole(StaffRole role);
 	
 }
