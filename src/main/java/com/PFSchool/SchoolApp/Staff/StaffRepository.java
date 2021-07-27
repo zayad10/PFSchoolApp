5d032cf92d5fd0104638103a880e9de25109b17b
Zayad <zayadk10@gmail.com>
2021-07-28 00:43:25 +0100

    Added a staff repository class that creates queries for the entity table.

---
+++ src/main/java/com/PFSchool/SchoolApp/Staff/StaffRepository.java
@@ -0,0 +1,9 @@
+package com.PFSchool.SchoolApp.Staff;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffRepository extends JpaRepository<Staff, Integer> {
+	
+}
