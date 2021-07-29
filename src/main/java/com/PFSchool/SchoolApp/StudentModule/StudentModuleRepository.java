c5237aa639c51a517981bbee55449745e094d14e
James1849 <zhac362@live.rhul.ac.uk>
2021-07-29 18:07:47 +0100

    Add StudentModuleRepository.

---
+++ src/main/java/com/PFSchool/SchoolApp/StudentModule/StudentModuleRepository.java
@@ -0,0 +1,7 @@
+package com.PFSchool.SchoolApp.StudentModule;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentModuleRepository extends JpaRepository<StudentModule, Integer> {
+
+}
