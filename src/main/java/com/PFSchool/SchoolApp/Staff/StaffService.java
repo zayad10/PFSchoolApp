21ed4b1ea755b615b9fcbc44ff904636504ad8ec
Zayad Khan <zayadk10@gmail.com>
2021-07-29 23:58:31 +0100

    Added a staff service class to handle the business logic for staff queries.

---
+++ src/main/java/com/PFSchool/SchoolApp/Staff/StaffService.java
@@ -0,0 +1,14 @@
+package com.PFSchool.SchoolApp.Staff;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StaffService {
+	
+	@Autowired
+	private StaffRepository staffRepository;
+	
+	
+
+}
