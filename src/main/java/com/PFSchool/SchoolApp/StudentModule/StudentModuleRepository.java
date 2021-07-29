46ea3a94e8eb82d82ae74777095a4f613b5990e0
James1849 <zhac362@live.rhul.ac.uk>
2021-07-29 18:15:18 +0100

    Add @Repository annotation to studentModuleRepository.

--- src/main/java/com/PFSchool/SchoolApp/StudentModule/StudentModuleRepository.java
+++ src/main/java/com/PFSchool/SchoolApp/StudentModule/StudentModuleRepository.java
@@ -1,7 +1,9 @@
 package com.PFSchool.SchoolApp.StudentModule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentModuleRepository extends JpaRepository<StudentModule, Integer> {
 
 }
