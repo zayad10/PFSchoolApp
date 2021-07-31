3c560ffb3daae5ea96401cea5900d9a3dc3c6af3
Zayad Khan <zayadk10@gmail.com>
2021-07-31 19:38:06 +0100

    Added a method to get all students.

--- src/main/java/com/PFSchool/SchoolApp/Student/StudentService.java
+++ src/main/java/com/PFSchool/SchoolApp/Student/StudentService.java
@@ -1,7 +1,11 @@
 package com.PFSchool.SchoolApp.Student;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.PFSchool.SchoolApp.Staff.Staff;
 
 @Service
 public class StudentService {
@@ -9,6 +13,8 @@ public class StudentService {
 	@Autowired
 	StudentRepository studentRepository;
 	
-	
+	public List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
 	
 }
