e783c1aa747aaa43b42af569bbec578231e8a952
Zayad Khan <zayadk10@gmail.com>
2021-07-31 19:36:09 +0100

    Added the attributes for the student service class.

--- src/main/java/com/PFSchool/SchoolApp/Student/StudentService.java
+++ src/main/java/com/PFSchool/SchoolApp/Student/StudentService.java
@@ -1,8 +1,14 @@
 package com.PFSchool.SchoolApp.Student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+	
+	
 }
