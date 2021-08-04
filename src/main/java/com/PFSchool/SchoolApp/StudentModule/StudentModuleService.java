0a24892dd6c76631b317fa1aecf77b8592a4a6b3
James1849 <zhac362@live.rhul.ac.uk>
2021-08-04 21:30:22 +0100

    Add javadoc to StudentModule.

--- src/main/java/com/PFSchool/SchoolApp/StudentModule/StudentModuleService.java
+++ src/main/java/com/PFSchool/SchoolApp/StudentModule/StudentModuleService.java
@@ -4,16 +4,31 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for student relation with a module
+ * 
+ * @author James
+ */
 @Service
 public class StudentModuleService {
   
   @Autowired
   private StudentModuleRepository studentModuleRepository;
 
+  /**
+   * Add student to a module.
+   * 
+   * @param studentModule
+   */
   public void addStudentModule(StudentModule studentModule) {
     studentModuleRepository.save(studentModule);
   }
   
+  /**
+   * Gets list of all student module relations.
+   * 
+   * @return list of student modules
+   */
   public List<StudentModule> getAllStudentModules() {
     return studentModuleRepository.findAll();
   }
