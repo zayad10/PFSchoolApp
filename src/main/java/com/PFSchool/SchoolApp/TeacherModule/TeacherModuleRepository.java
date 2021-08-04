d8cc5a00471e10404fc0b74eba70c58bc15219b3
James1849 <zhac362@live.rhul.ac.uk>
2021-08-04 21:19:10 +0100

    Add method to get teachers for module.

--- src/main/java/com/PFSchool/SchoolApp/TeacherModule/TeacherModuleRepository.java
+++ src/main/java/com/PFSchool/SchoolApp/TeacherModule/TeacherModuleRepository.java
@@ -1,9 +1,13 @@
 package com.PFSchool.SchoolApp.TeacherModule;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.PFSchool.SchoolApp.Module.Module;
 
 @Repository
 public interface TeacherModuleRepository extends JpaRepository<TeacherModule, Integer> {
  
+  public List<TeacherModule> findByModule(Module module);
+  
 }
--- src/main/java/com/PFSchool/SchoolApp/TeacherModule/TeacherModuleService.java
+++ src/main/java/com/PFSchool/SchoolApp/TeacherModule/TeacherModuleService.java
@@ -1,8 +1,11 @@
 package com.PFSchool.SchoolApp.TeacherModule;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.PFSchool.SchoolApp.Staff.Staff;
+import com.PFSchool.SchoolApp.Module.Module;
 
 @Service
 public class TeacherModuleService {
@@ -10,11 +13,25 @@ public class TeacherModuleService {
   @Autowired
   private TeacherModuleRepository teacherModuleRepository;
   
+  @Autowired
+  private TeacherModuleService teacherService;
+  
   public void addTeacherModule(TeacherModule teacherModule) {
     teacherModuleRepository.save(teacherModule);
   }
   
   public List<TeacherModule> getAllTeacherModule() {
     return teacherModuleRepository.findAll();
+  }
+  
+  public List<Staff> getStaffForModule(Module module) {
+    List<TeacherModule> teacherModules = teacherModuleRepository.findByModule(module);
+    List<Staff> modules = new ArrayList<Staff>();
+    
+    for (TeacherModule teacherModule : teacherModules) {
+      modules.add(teacherModule.getTeacher());
+    }
+    
+    return modules;
   }
 }
