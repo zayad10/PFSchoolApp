7f835a6328cbcdf0bab7ec255858b3b07795972c
James1849 <zhac362@live.rhul.ac.uk>
2021-08-10 11:44:19 +0100

    Add get modules for student mapping in studentModuleController.

--- src/main/java/com/PFSchool/SchoolApp/StudentModule/StudentModuleController.java
+++ src/main/java/com/PFSchool/SchoolApp/StudentModule/StudentModuleController.java
@@ -1,12 +1,20 @@
 package com.PFSchool.SchoolApp.StudentModule;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.PFSchool.SchoolApp.Module.Module;
 
 @RestController
 public class StudentModuleController {
   
   @Autowired
   private StudentModuleService studentModuleService;
-
+  
+  @RequestMapping("get-modules-for-student/{studentId}")
+  public List<Module> getStudentModules(@PathVariable Integer studentId) {
+    return studentModuleService.getModulesForStudent(studentId);
+  }
 }
