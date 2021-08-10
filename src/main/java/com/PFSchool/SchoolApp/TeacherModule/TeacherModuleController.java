a6a8263f9f9331b78a8e159c6f5115d1f4d25290
James1849 <zhac362@live.rhul.ac.uk>
2021-08-10 11:47:48 +0100

    Add getModulesForStaff mapping to controller.

--- src/main/java/com/PFSchool/SchoolApp/TeacherModule/TeacherModuleController.java
+++ src/main/java/com/PFSchool/SchoolApp/TeacherModule/TeacherModuleController.java
@@ -1,12 +1,20 @@
 package com.PFSchool.SchoolApp.TeacherModule;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.PFSchool.SchoolApp.Module.Module;
 
 @RestController
 public class TeacherModuleController {
   
   @Autowired
   private TeacherModuleService teacherModuleService;
-
+  
+  @RequestMapping("get-modules-for-staff/{staffId}")
+  public List<Module> getModulesForStaff(@PathVariable Integer staffId) {
+    return teacherModuleService.getModuleForStaff(staffId);
+  }
 }
